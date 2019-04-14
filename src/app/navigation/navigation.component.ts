c083898f6da40093fb91dc1bebb3c1e426e2a043
Philippe Martin <contact@elol.fr>
2019-04-14 19:09:33 +0200

    Add Navigation and toolbar with material schematic:nav

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+  @Input() appname: string;
+  @Input() menuname: string;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
